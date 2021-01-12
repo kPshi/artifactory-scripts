9748e4b1711e3f61e4adbb4603f810fc54717537
kPshi <karoshi@137tmx.de>
2021-01-12 15:49:06 +0100

    consistent variable names

--- replicationDiff/replicationDiff.sh
+++ replicationDiff/replicationDiff.sh
@@ -5,88 +5,85 @@ set -o pipefail
 set -o nounset
 
 if [[ $# -lt 18 ]] && [[ $# -ne 0 ]] ; then
-echo
-echo "You have not passed all the required 9 parameters or one of the parameter is missing a value"
-echo "Please make sure that all of these parameters have a value -source_adminuser  -target_adminuser  -source_art  -target_art  -source_repo  -target_repo  -source_password  -target_password  -download_missingfiles"
-echo
-exit 0
+  echo
+  echo "You have not passed all the required 9 parameters or one of the parameter is missing a value"
+  echo
+  exit 0
 fi
 
 if [[ $# -gt 18 ]] ; then
-echo
-echo "You have not passed all the required 9 parameters or one of the parameter is missing a value"
-echo "Please make sure that all of these parameters have a value -source_adminuser  -target_adminuser  -source_art  -target_art  -source_repo  -target_repo  -source_password  -target_password  -download_missingfiles"
-echo
-exit 0
+  echo
+  echo "You have not passed all the required 9 parameters or one of the parameter is missing a value"
+  echo "Please make sure that all of these parameters have a value -source_adminuser  -target_adminuser  -source_art  -target_art  -source_repo  -target_repo  -source_password  -target_password  -download_missingfiles"
+  echo
+  exit 0
 fi
 
 if [[ $# -ne 0 ]] ; then
-while test $# -gt 0; do
-           case "$1" in
-                -source_adminuser)
-                    shift
-                    SOURCEUSER=$1
-                    shift
-                    ;;
-                -target_adminuser)
-                    shift
-                    TARGETUSER=$1
-                    shift
-                    ;;
-                -source_art)
-                    shift
-                    SOURCEART=$1
-                    shift
-                    ;;
-                -target_art)
-                    shift
-                    TARGETART=$1
-                    shift
-                    ;;
-                -source_repo)
-                    shift
-                    SOURCEREPO=$1
-                    shift
-                    ;;
-                -target_repo)
-                    shift
-                    TARGETREPO=$1
-                    shift
-                    ;;
-                -source_password)
-                    shift
-                    SOURCEPASSWORD=$1
-                    shift
-                    ;;
-                -target_password)
-                    shift
-                    TARGETPASSWORD=$1
-                    shift
-                    ;;
-                -download_missingfiles)
-                    shift
-                    DOWNLOADMISSINGFILES=$1
-                    shift
-                    ;;
+  while test $# -gt 0; do
+    case "$1" in
+      -source_adminuser)
+        shift
+        source_adminuser=$1
+        shift
+        ;;
+      -target_adminuser)
+        shift
+        target_adminuser=$1
+        shift
+        ;;
+      -source_art)
+        shift
+        source_art=$1
+        shift
+        ;;
+      -target_art)
+        shift
+        target_art=$1
+        shift
+        ;;
+      -source_repo)
+        shift
+        source_repo=$1
+        shift
+        ;;
+      -target_repo)
+        shift
+        target_repo=$1
+        shift
+        ;;
+      -source_password)
+        shift
+        source_password=$1
+        shift
+        ;;
+      -target_password)
+        shift
+        target_password=$1
+        shift
+        ;;
+      -download_missingfiles)
+        shift
+        download_missingfiles=$1
+        shift
+        ;;
+      *)
+        echo 
+        echo "$1 is not a recognized flag!"
+        exit 0 
+        # return 1;
+        ;;
+    esac
+  done
 
-                *)
-                  echo 
-                  echo "$1 is not a recognized flag!"
-                  exit 0 
-                  # return 1;
-                   ;;
-          esac
-          done
-
-
-#  echo "SOURCE_USER : $SOURCEUSER";
-#  echo "TARGET_USER : $TARGETUSER";
+#  echo "SOURCE_USER : $source_adminuser";
+#  echo "TARGET_USER : $target_adminuser";
  
- SOURCEART=${SOURCEART%/}
- TARGETART=${TARGETART%/}
+ source_art=${source_art%/}
+ target_art=${target_art%/}
 
 
-status_code=$(curl -u$SOURCEUSER:$SOURCEPASSWORD --write-out %{http_code} --silent --output /dev/null "$SOURCEART/api/storage/$SOURCEREPO/?list&deep=1&listFolders=0&mdTimestamps=1&statsTimestamps=1&includeRootPath=1" -L)
+status_code=$(curl -u$source_adminuser:$source_password --write-out %{http_code} --silent --output /dev/null "$source_art/api/storage/$source_repo/?list&deep=1&listFolders=0&mdTimestamps=1&statsTimestamps=1&includeRootPath=1" -L)
 
 if [[ "$status_code" -eq 401 ]] && [[ "$status_code" -ne 200 ]]
   then
@@ -99,7 +96,7 @@ fi
 if [[ "$status_code" -eq 000 ]] && [[ "$status_code" -ne 200 ]] 
   then
   echo
-  echo "Request failed with Could not resolve host: $SOURCEART Please check the Source Artifactory URL (-source_art) and make sure its correct"
+  echo "Request failed with Could not resolve host: $source_art Please check the Source Artifactory URL (-source_art) and make sure its correct"
   echo
   exit 0
 fi
@@ -128,7 +125,7 @@ if [[ "$status_code" -ne 200 ]]
   exit 0
 fi
 
-status_code=$(curl -u$TARGETUSER:$TARGETPASSWORD --write-out %{http_code} --silent --output /dev/null "$TARGETART/api/storage/$TARGETREPO/?list&deep=1&listFolders=0&mdTimestamps=1&statsTimestamps=1&includeRootPath=1" -L)
+status_code=$(curl -u$target_adminuser:$target_password --write-out %{http_code} --silent --output /dev/null "$target_art/api/storage/$target_repo/?list&deep=1&listFolders=0&mdTimestamps=1&statsTimestamps=1&includeRootPath=1" -L)
 
 if [[ "$status_code" -eq 401 ]] && [[ "$status_code" -ne 200 ]]
   then
@@ -141,7 +138,7 @@ fi
 if [[ "$status_code" -eq 000 ]] && [[ "$status_code" -ne 200 ]]
   then
   echo
-  echo "Request failed with Could not resolve host: $TARGETART Please check the Target Artifactory URL (-target_art) and make sure its correct"
+  echo "Request failed with Could not resolve host: $target_art Please check the Target Artifactory URL (-target_art) and make sure its correct"
   echo
   exit 0
 fi
@@ -170,12 +167,12 @@ if [[ "$status_code" -ne 200 ]]
   exit 0
 fi
 
-curl -X GET -u$SOURCEUSER:$SOURCEPASSWORD "$SOURCEART/api/storage/$SOURCEREPO/?list&deep=1&listFolders=0&mdTimestamps=1&statsTimestamps=1&includeRootPath=1" -L > source.log
-curl -X GET -u$TARGETUSER:$TARGETPASSWORD "$TARGETART/api/storage/$TARGETREPO/?list&deep=1&listFolders=0&mdTimestamps=1&statsTimestamps=1&includeRootPath=1" -L > target.log
+curl -X GET -u$source_adminuser:$source_password "$source_art/api/storage/$source_repo/?list&deep=1&listFolders=0&mdTimestamps=1&statsTimestamps=1&includeRootPath=1" -L > source.log
+curl -X GET -u$target_adminuser:$target_password "$target_art/api/storage/$target_repo/?list&deep=1&listFolders=0&mdTimestamps=1&statsTimestamps=1&includeRootPath=1" -L > target.log
 
 diff --new-line-format="" --unchanged-line-format=""  source.log target.log > diff_output.txt
 sed -n '/uri/p' diff_output.txt | sed 's/[<>,]//g' | sed '/https/d' | sed '/http/d' | sed  's/ //g' | sed 's/[",]//g' | sed 's/uri://g' > cleanpaths.txt
-prefix=$SOURCEART/$SOURCEREPO
+prefix=$source_art/$source_repo
 awk -v prefix="$prefix" '{print prefix $0}' cleanpaths.txt > filepaths_uri.txt
 
 echo
@@ -187,13 +184,13 @@ sed '/maven-metadata.xml/d' filepaths_uri.txt |  sed '/Packages.bz2/d' | sed '/.
 rm source.log target.log diff_output.txt cleanpaths.txt
 echo
 
-if [[ $DOWNLOADMISSINGFILES =~ [yY](es)* ]] ; then
+if [[ $download_missingfiles =~ [yY](es)* ]] ; then
 mkdir replication_downloads
 cd replication_downloads
-cat ../filepaths_nometadatafiles.txt | xargs -n 1 curl -sS -L -O -u$SOURCEUSER:$SOURCEPASSWORD
+cat ../filepaths_nometadatafiles.txt | xargs -n 1 curl -sS -L -O -u$source_adminuser:$source_password
 echo "Downloading all the files that are present in Source repository and missing from the Target repository to a folder '"replication_downloads"' in the current working directory"
 fi
-if [[ $DOWNLOADMISSINGFILES =~ [nN](o)* ]] ; then
+if [[ $download_missingfiles =~ [nN](o)* ]] ; then
 exit 0
 fi
 exit 0
@@ -201,25 +198,25 @@ fi
 
 if [[ $# -eq 0 ]] ; then
 echo "Enter your source Artifactory URL: "
-read Source_ART_URL
-SOURCE_ART=${Source_ART_URL%/}
+read source_art
+source_art=${source_art%/}
 echo "Enter your target Artifactory URL: "
-read Target_ART_URL
-TARGET_ART=${Target_ART_URL%/}
+read target_art
+target_art=${target_art%/}
 echo "Enter your source repository name: "
-read Source_repo_name
+read source_repo
 echo "Enter your target repository name: "
-read Target_repo_name
+read target_repo
 echo "Enter admin username for source Artifactory: "
-read source_username
+read source_adminuser
 echo "Password for source Artifactory: "
 read -s source_password
 echo "Enter admin username for target Artifactory: "
-read target_username
+read target_adminuser
 echo "Password for target Artifactory: "
 read -s target_password
 
-status_code=$(curl -u$source_username:$source_password --write-out %{http_code} --silent --output /dev/null "$SOURCE_ART/api/storage/$Source_repo_name/?list&deep=1&listFolders=0&mdTimestamps=1&statsTimestamps=1&includeRootPath=1" -L)
+status_code=$(curl -u$source_adminuser:$source_password --write-out %{http_code} --silent --output /dev/null "$source_art/api/storage/${source_repo}/?list&deep=1&listFolders=0&mdTimestamps=1&statsTimestamps=1&includeRootPath=1" -L)
 
 if [[ "$status_code" -eq 401 ]] && [[ "$status_code" -ne 200 ]]
   then
@@ -232,7 +229,7 @@ fi
 if [[ "$status_code" -eq 000 ]] && [[ "$status_code" -ne 200 ]]
   then
   echo
-  echo "Request failed with Could not resolve host: $SOURCE_ART. Please check the Source Artifactory URL and make sure its correct"
+  echo "Request failed with Could not resolve host: $source_art. Please check the Source Artifactory URL and make sure its correct"
   echo
   exit 0
 fi
@@ -261,7 +258,7 @@ if [[ "$status_code" -ne 200 ]]
   exit 0
 fi
 
-status_code=$(curl -u$target_username:$target_password --write-out %{http_code} --silent --output /dev/null "$TARGET_ART/api/storage/$Target_repo_name/?list&deep=1&listFolders=0&mdTimestamps=1&statsTimestamps=1&includeRootPath=1" -L)
+status_code=$(curl -u$target_adminuser:$target_password --write-out %{http_code} --silent --output /dev/null "$target_art/api/storage/${target_repo}/?list&deep=1&listFolders=0&mdTimestamps=1&statsTimestamps=1&includeRootPath=1" -L)
 
 if [[ "$status_code" -eq 401 ]] && [[ "$status_code" -ne 200 ]]
   then
@@ -274,7 +271,7 @@ fi
 if [[ "$status_code" -eq 000 ]] && [[ "$status_code" -ne 200 ]]
   then
   echo
-  echo "Request failed with Could not resolve host: $TARGET_ART Please check the Target Artifactory URL and make sure its correct"
+  echo "Request failed with Could not resolve host: $target_art Please check the Target Artifactory URL and make sure its correct"
   echo
   exit 0
 fi
@@ -303,11 +300,11 @@ if [[ "$status_code" -ne 200 ]]
   exit 0
 fi
 
-curl -X GET -u$source_username:$source_password "$SOURCE_ART/api/storage/$Source_repo_name/?list&deep=1&listFolders=0&mdTimestamps=1&statsTimestamps=1&includeRootPath=1" -L > source.log
-curl -X GET -u$target_username:$target_password "$TARGET_ART/api/storage/$Target_repo_name/?list&deep=1&listFolders=0&mdTimestamps=1&statsTimestamps=1&includeRootPath=1" -L > target.log
+curl -X GET -u$source_adminuser:$source_password "$source_art/api/storage/${source_repo}/?list&deep=1&listFolders=0&mdTimestamps=1&statsTimestamps=1&includeRootPath=1" -L > source.log
+curl -X GET -u$target_adminuser:$target_password "$target_art/api/storage/${target_repo}/?list&deep=1&listFolders=0&mdTimestamps=1&statsTimestamps=1&includeRootPath=1" -L > target.log
 diff --new-line-format="" --unchanged-line-format=""  source.log target.log > diff_output.txt
 sed -n '/uri/p' diff_output.txt | sed 's/[<>,]//g' | sed '/https/d' | sed '/http/d' | sed  's/ //g' | sed 's/[",]//g' | sed 's/uri://g' > cleanpaths.txt
-prefix=$SOURCE_ART/$Source_repo_name
+prefix=$source_art/${source_repo}
 awk -v prefix="$prefix" '{print prefix $0}' cleanpaths.txt > filepaths_uri.txt
 
 echo
@@ -330,5 +327,5 @@ exit 0
 fi
 mkdir replication_downloads
 cd replication_downloads
-cat ../filepaths_nometadatafiles.txt | xargs -n 1 curl -sS -L -O -u$source_username:$source_password
+cat ../filepaths_nometadatafiles.txt | xargs -n 1 curl -sS -L -O -u$source_adminuser:$source_password
 fi
