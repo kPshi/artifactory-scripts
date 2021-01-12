db125fd1b68c1f5a73b30d4665f06292d556b81b
kPshi <karoshi@137tmx.de>
2021-01-12 18:12:42 +0100

    proper option handling

--- replicationDiff/replicationDiff.sh
+++ replicationDiff/replicationDiff.sh
@@ -4,308 +4,186 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-if [[ $# -lt 18 ]] && [[ $# -ne 0 ]] ; then
+die() {
   echo
-  echo "You have not passed all the required 9 parameters or one of the parameter is missing a value"
-  echo
-  exit 1
-fi
-
-if [[ $# -gt 18 ]] ; then
-  echo
-  echo "You have not passed all the required 9 parameters or one of the parameter is missing a value"
-  echo "Please make sure that all of these parameters have a value -source_adminuser  -target_adminuser  -source_art  -target_art  -source_repo  -target_repo  -source_password  -target_password  -download_missingfiles"
+  [ "${*:-}" ] && cat <<<"$*" || cat
+  echo "$*"
   echo
   exit 1
+}
+
+handle_http_status() {
+  local -i code="$1"; shift
+  local which="$1"; shift
+
+  case "${code}" in
+    200)
+      return
+      ;;
+    401)
+      die "Request failed with HTTP ${status_code}. Please check the provided username (-${which}_adminuser) and password (-${which}_password) for the Target Artifactory"
+      ;;
+    *)
+      die "Request failed with HTTP ${status_code}. Please check the Target Artifactory URL (-${which}_art) and Target Repository (-${which}_repo) make sure its correct."
+  esac
+}
+
+if [ "$#" = 0 ] ; then
+  help_wanted=1
 fi
 
-if [[ $# -ne 0 ]] ; then
-  while test $# -gt 0; do
-    case "$1" in
-      -source_adminuser)
-        shift
-        source_adminuser=$1
-        shift
-        ;;
-      -target_adminuser)
-        shift
-        target_adminuser=$1
-        shift
-        ;;
-      -source_art)
-        shift
-        source_art=$1
-        shift
-        ;;
-      -target_art)
-        shift
-        target_art=$1
-        shift
-        ;;
-      -source_repo)
-        shift
-        source_repo=$1
-        shift
-        ;;
-      -target_repo)
-        shift
-        target_repo=$1
-        shift
-        ;;
-      -source_password)
-        shift
-        source_password=$1
-        shift
-        ;;
-      -target_password)
-        shift
-        target_password=$1
-        shift
-        ;;
-      -download_missingfiles)
-        shift
-        download_missingfiles=$1
-        shift
-        ;;
-      *)
-        echo 
-        echo "$1 is not a recognized flag!"
-        exit 1 
-        # return 1;
-        ;;
-    esac
-  done
+args="$(getopt \
+          --name "$(basename "$0")" \
+          --alternative \
+          --options 'h' \
+          --longoptions source_adminuser:,target_adminuser:,source_art:,target_art:,source_repo:,target_repo:,source_password:,target_password:,download_missingfiles:,help \
+          -- \
+          "$@"
+      )"
+eval set -- "${args}"
 
-  #  echo "SOURCE_USER : ${source_adminuser}";
-  #  echo "TARGET_USER : ${target_adminuser}";
-   
-   source_art="${source_art%/}"
-   target_art="${target_art%/}"
-  
-  
-  status_code=$(curl --user "${source_adminuser}:${source_password}" --write-out %{http_code} --silent --output /dev/null "${source_art}/api/storage/${source_repo}/?list&deep=1&listFolders=0&mdTimestamps=1&statsTimestamps=1&includeRootPath=1" --location)
-  
-  if [[ "${status_code}" -eq 401 ]] ; then
-    echo
-    echo "Request failed with HTTP ${status_code}. Please check the provided username (-source_adminuser) and password (-source_password) for the Source Artifactory" 
-    echo
-    exit 1
-  fi
-  
-  if [[ "${status_code}" -eq 000 ]] ; then
-    echo
-    echo "Request failed with Could not resolve host: ${source_art} Please check the Source Artifactory URL (-source_art) and make sure its correct"
-    echo
-    exit 1
-  fi
-  
-  if [[ "${status_code}" -eq 404 ]] ; then
-    echo
-    echo "Request failed with HTTP ${status_code}. Please check the Source Artifactory URL (-source_art) and Source Repository (-source_repo) make sure its correct. "
-    echo
-    exit 1
-  fi
-  
-  if [[ "${status_code}" -eq 400 ]] ; then
-    echo
-    echo "Request failed with HTTP ${status_code}. Please check the Source Artifactory URL (-source_art) and Source Repository (-source_repo) make sure its correct. "
-    echo
-    exit 1
-  fi
-  
-  if [[ "${status_code}" -ne 200 ]] ; then
-    echo
-    echo "Request failed with HTTP ${status_code}. Please check the Source Artifactory URL (-source_art) and Source Repository (-source_repo) make sure its correct."
-    echo
-    exit 1
-  fi
-  
-  status_code=$(curl --user "${target_adminuser}:${target_password}" --write-out %{http_code} --silent --output /dev/null "${target_art}/api/storage/${target_repo}/?list&deep=1&listFolders=0&mdTimestamps=1&statsTimestamps=1&includeRootPath=1" --location)
-  
-  if [[ "${status_code}" -eq 401 ]] ; then
-    echo
-    echo "Request failed with HTTP ${status_code}. Please check the provided username (-target_adminuser) and password (-target_password) for the Target Artifactory"
-    echo
-    exit 1
-  fi
-  
-  if [[ "${status_code}" -eq 000 ]] ; then
-    echo
-    echo "Request failed with Could not resolve host: ${target_art} Please check the Target Artifactory URL (-target_art) and make sure its correct"
-    echo
-    exit 1
-  fi
-  
-  if [[ "${status_code}" -eq 404 ]] ; then
-    echo
-    echo "Request failed with HTTP ${status_code}. Please check the Target Artifactory URL (-target_art) and Target Repository (-target_repo) make sure its correct. "
-    echo
-    exit 1
-  fi
-  
-  if [[ "${status_code}" -eq 400 ]] ; then
-    echo
-    echo "Request failed with HTTP ${status_code}. Please check the Target Artifactory URL (-target_art) and Target Repository (-target_repo) make sure its correct. "
-    echo
-    exit 1
-  fi
-  
-  if [[ "${status_code}" -ne 200 ]] ; then
-    echo
-    echo "Request failed with HTTP ${status_code}. Please check the Target Artifactory URL (-target_art) and Target Repository (-target_repo) make sure its correct."
-    echo
-    exit 1
-  fi
-  
-  curl --request GET --user "${source_adminuser}:${source_password}" "${source_art}/api/storage/${source_repo}/?list&deep=1&listFolders=0&mdTimestamps=1&statsTimestamps=1&includeRootPath=1" --location > source.log
-  curl --request GET --user "${target_adminuser}:${target_password}" "${target_art}/api/storage/${target_repo}/?list&deep=1&listFolders=0&mdTimestamps=1&statsTimestamps=1&includeRootPath=1" --location > target.log
-  
-  diff --new-line-format="" --unchanged-line-format=""  source.log target.log > diff_output.txt
-  sed --quiet '/uri/p' diff_output.txt | sed 's/[<>,]//g' | sed '/https/d' | sed '/http/d' | sed  's/ //g' | sed 's/[",]//g' | sed 's/uri://g' > cleanpaths.txt
-  prefix="${source_art}/${source_repo}"
-  awk -v prefix="${prefix}" '{print prefix $0}' cleanpaths.txt > filepaths_uri.txt
-  
+while [ "$1" != -- ] ; do
+  case "$1" in
+    --source_adminuser)
+      source_adminuser=$2
+      shift
+      ;;
+    --target_adminuser)
+      target_adminuser=$2
+      shift
+      ;;
+    --source_art)
+      source_art=$2
+      shift
+      ;;
+    --target_art)
+      target_art=$2
+      shift
+      ;;
+    --source_repo)
+      source_repo=$2
+      shift
+      ;;
+    --target_repo)
+      target_repo=$2
+      shift
+      ;;
+    --source_password)
+      source_password=$2
+      shift
+      ;;
+    --target_password)
+      target_password=$2
+      shift
+      ;;
+    --download_missingfiles)
+      download_missingfiles=$2
+      shift
+      ;;
+    --help|-h)
+      help_wanted=1
+      ;;
+    *)
+      die "$1 is not a recognized flag!"
+      ;;
+  esac
+  shift
+done
+shift
+
+if [ $# != 0 ] ; then
+  die "Extra parameters: $*"
+fi
+
+if [ "${help_wanted:-}" ] ; then
+  die <<-END
+Syntax: $(basename "$0") <options>
+Where options are
+  --source_adminuser <name>       username to authenticate with the source
+  --source_art <url>              base URL of the source artifactory
+  --source_repo <name>            which source repo to compare
+  --source_password <password>    password fpr the source Artifactory
+  --target_adminuser <name>       username to authenticate with the target
+  --target_art <url>              base URL of the target artifactory
+  --target_repo <name>            which target repo to compare
+  --target_password <password>    password fpr the target Artifactory
+  --download_missingfiles=[yn]    download files missing in the target repository
+  --help|-h                       show this help
+END
+fi
+
+if [ "${source_art:-}" = "" ] ; then
+  echo "Enter your source Artifactory URL: "
+  read source_art
+fi
+
+if [ "${target_art:-}" = "" ] ; then
+  echo "Enter your target Artifactory URL: "
+  read target_art
+fi
+
+if [ "${source_repo:-}" = "" ] ; then
+  echo "Enter your source repository name: "
+  read source_repo
+fi
+
+if [ "${target_repo:-}" = "" ] ; then
+  echo "Enter your target repository name: "
+  read target_repo
+fi
+
+if [ "${source_adminuser:-}" = "" ] ; then
+  echo "Enter admin username for source Artifactory: "
+  read source_adminuser
+fi
+
+if [ "${source_password:-}" = "" ] ; then
+  echo "Password for source Artifactory: "
+  read -s source_password
+fi
+
+if [ "${target_adminuser:-}" = "" ] ; then
+  echo "Enter admin username for target Artifactory: "
+  read target_adminuser
+fi
+
+if [ "${target_password:-}" = "" ] ; then
+  echo "Password for target Artifactory: "
+  read -s target_password
+fi
+
+source_art="${source_art%/}"
+target_art="${target_art%/}"
+
+
+trap 'rm source.list target.list diff_output.txt cleanpaths.txt' EXIT
+
+status_code="$(curl --request GET --user "${source_adminuser}:${source_password}" "${source_art}/api/storage/${source_repo}/?list&deep=1&listFolders=0&mdTimestamps=1&statsTimestamps=1&includeRootPath=1" --location --output source.list --write-out %{http_code} --silent 2>/dev/null ||:)"
+handle_http_status "${status_code}" source
+
+status_code="$(curl --request GET --user "${target_adminuser}:${target_password}" "${target_art}/api/storage/${target_repo}/?list&deep=1&listFolders=0&mdTimestamps=1&statsTimestamps=1&includeRootPath=1" --location --output target.list --write-out %{http_code} --silent 2>/dev/null ||:)"
+handle_http_status "${status_code}" target
+
+diff --new-line-format="" --unchanged-line-format="" source.list target.list > diff_output.txt
+grep uri diff_output.txt | sed 's/[<>,]//g; /https/d; /http/d; s/ //g; s/[",]//g; s/uri://g' > cleanpaths.txt
+awk -v prefix="${source_art}/${source_repo}" '{print prefix $0}' cleanpaths.txt > filepaths_uri.txt
+
+if [ "${count_extensions}" ] ; then
   echo
   echo
   echo "Here is the count of files sorted according to the file extension that are present in the source repository and are missing in the target repository. Please note that if there are SHA files in these repositories which will have no extension, then the entire URL will be seen in the output. The SHA files will be seen for docker repositories whose layers are named as per their SHA value. "
   echo
   grep --extended-regexp ".*\.[a-zA-Z0-9]*$" filepaths_uri.txt | sed --expression 's/.*\(\.[a-zA-Z0-9]*\)$/\1/' filepaths_uri.txt | sort | uniq --count | sort -n
   sed '/maven-metadata.xml/d' filepaths_uri.txt |  sed '/Packages.bz2/d' | sed '/.*gemspec.rz$/d' |  sed '/Packages.gz/d' | sed '/Release/d' | sed '/.*json$/d' | sed '/Packages/d' | sed '/by-hash/d' | sed '/filelists.xml.gz/d' | sed '/other.xml.gz/d' | sed '/primary.xml.gz/d' | sed '/repomd.xml/d' | sed '/repomd.xml.asc/d' | sed '/repomd.xml.key/d' > filepaths_nometadatafiles.txt
-  rm source.log target.log diff_output.txt cleanpaths.txt
   echo
-  
-  if [[ "${download_missingfiles}" =~ [yY](es)* ]] ; then
-    mkdir replication_downloads
-    cd replication_downloads
-    cat ../filepaths_nometadatafiles.txt | xargs --max-args 1 curl --silent --show-error --location --remote-name --user "${source_adminuser}:${source_password}"
-    echo "Downloading all the files that are present in Source repository and missing from the Target repository to a folder '"replication_downloads"' in the current working directory"
-  fi
-  if [[ "${download_missingfiles}" =~ [nN](o)* ]] ; then
-    exit 0
-  fi
-  exit 0
 fi
 
-if [[ $# -eq 0 ]] ; then
-  echo "Enter your source Artifactory URL: "
-  read source_art
-  source_art="${source_art%/}"
-  echo "Enter your target Artifactory URL: "
-  read target_art
-  target_art="${target_art%/}"
-  echo "Enter your source repository name: "
-  read source_repo
-  echo "Enter your target repository name: "
-  read target_repo
-  echo "Enter admin username for source Artifactory: "
-  read source_adminuser
-  echo "Password for source Artifactory: "
-  read -s source_password
-  echo "Enter admin username for target Artifactory: "
-  read target_adminuser
-  echo "Password for target Artifactory: "
-  read -s target_password
-  
-  status_code=$(curl --user "${source_adminuser}:${source_password}" --write-out %{http_code} --silent --output /dev/null "${source_art}/api/storage/${source_repo}/?list&deep=1&listFolders=0&mdTimestamps=1&statsTimestamps=1&includeRootPath=1" --location)
-  
-  if [[ "${status_code}" -eq 401 ]] ; then
-    echo
-    echo "Request failed with HTTP ${status_code}. Please check the provided admin username and password for the Source Artifactory"
-    echo
-    exit 1
-  fi
-  
-  if [[ "${status_code}" -eq 000 ]] ; then
-    echo
-    echo "Request failed with Could not resolve host: ${source_art}. Please check the Source Artifactory URL and make sure its correct"
-    echo
-    exit 1
-  fi
-  
-  if [[ "${status_code}" -eq 404 ]] ; then
-    echo
-    echo "Request failed with HTTP ${status_code}. Please check the Source Artifactory URL and Source Repository name provided make sure its correct. "
-    echo
-    exit 1
-  fi
-  
-  if [[ "${status_code}" -eq 400 ]] ; then
-    echo
-    echo "Request failed with HTTP ${status_code}. Please check the Source Artifactory URL and Source Repository name provided make sure its correct. "
-    echo
-    exit 1
-  fi
-  
-  if [[ "${status_code}" -ne 200 ]] ; then
-    echo
-    echo "Request failed with HTTP ${status_code}. Please check the Source Artifactory URL and Source Repository name provided make sure its correct."
-    echo
-    exit 1
-  fi
-  
-  status_code=$(curl --user "${target_adminuser}:${target_password}" --write-out %{http_code} --silent --output /dev/null "${target_art}/api/storage/${target_repo}/?list&deep=1&listFolders=0&mdTimestamps=1&statsTimestamps=1&includeRootPath=1" --location)
-  
-  if [[ "${status_code}" -eq 401 ]] ; then
-    echo
-    echo "Request failed with HTTP ${status_code}. Please check the provided admin username and password for the Target Artifactory"
-    echo
-    exit 1
-  fi
-  
-  if [[ "${status_code}" -eq 000 ]] ; then
-    echo
-    echo "Request failed with Could not resolve host: ${target_art} Please check the Target Artifactory URL and make sure its correct"
-    echo
-    exit 1
-  fi
-  
-  if [[ "${status_code}" -eq 404 ]] ; then
-    echo
-    echo "Request failed with HTTP ${status_code}. Please check the Target Artifactory URL and Target Repository name provided make sure its correct. "
-    echo
-    exit 1
-  fi
-  
-  if [[ "${status_code}" -eq 400 ]] ; then
-    echo
-    echo "Request failed with HTTP ${status_code}. Please check the Target Artifactory URL and Target Repository name provided make sure its correct. "
-    echo
-    exit 1
-  fi
-  
-  if [[ "${status_code}" -ne 200 ]] ; then
-    echo
-    echo "Request failed with HTTP ${status_code}. Please check the Target Artifactory URL and Target Repository name provided make sure its correct. "
-    echo
-    exit 1
-  fi
-  
-  curl --request GET --user "${source_adminuser}:${source_password}" "${source_art}/api/storage/${source_repo}/?list&deep=1&listFolders=0&mdTimestamps=1&statsTimestamps=1&includeRootPath=1" --location > source.log
-  curl --request GET --user "${target_adminuser}:${target_password}" "${target_art}/api/storage/${target_repo}/?list&deep=1&listFolders=0&mdTimestamps=1&statsTimestamps=1&includeRootPath=1" --location > target.log
-  diff --new-line-format="" --unchanged-line-format=""  source.log target.log > diff_output.txt
-  sed --quiet '/uri/p' diff_output.txt | sed 's/[<>,]//g' | sed '/https/d' | sed '/http/d' | sed  's/ //g' | sed 's/[",]//g' | sed 's/uri://g' > cleanpaths.txt
-  prefix="${source_art}/${source_repo}"
-  awk -v prefix="${prefix}" '{print prefix $0}' cleanpaths.txt > filepaths_uri.txt
-  
-  echo
-  echo
-  echo "Here is the count of files sorted according to the file extension that are present in the source repository and are missing in the target repository. Please note that if there are SHA files in these repositories which will have no extension, then the entire URL will be seen in the output. The SHA files will be seen for docker repositories whose layers are named as per their SHA value. In the case of Debian repositories you will see SHA files and also metadata files with entire URL in the output as they have no extension. "
-  echo
-  grep --extended-regexp ".*\.[a-zA-Z0-9]*$" filepaths_uri.txt | sed --expression 's/.*\(\.[a-zA-Z0-9]*\)$/\1/' filepaths_uri.txt | sort | uniq --count | sort -n
-  sed '/maven-metadata.xml/d' filepaths_uri.txt |  sed '/Packages.bz2/d' | sed '/.*gemspec.rz$/d' |  sed '/Packages.gz/d' | sed '/Release/d' | sed '/.*json$/d' | sed '/Packages/d' | sed '/by-hash/d' | sed '/filelists.xml.gz/d' | sed '/other.xml.gz/d' | sed '/primary.xml.gz/d' | sed '/repomd.xml/d' | sed '/repomd.xml.asc/d' | sed '/repomd.xml.key/d' > filepaths_nometadatafiles.txt
-  rm source.log target.log diff_output.txt cleanpaths.txt
-  echo
-  echo
-  echo "Do you want to download all the files that are present in Source repository and missing from the Target repository?(yes/no)"
-  read input
-  if [[ "${input}" =~ [yY](es)* ]] ; then
-    echo "Downloading the missing files to a folder '"replication_downloads"' in the current working directory"
-  fi
-  if [[ "${input}" =~ [nN](o)* ]] ; then
-    echo "done"
-    exit 0
-  fi
+
+if [[ "${download_missingfiles,,:-}" =~ ^y(es)?$ ]] ; then
   mkdir replication_downloads
   cd replication_downloads
   cat ../filepaths_nometadatafiles.txt | xargs --max-args 1 curl --silent --show-error --location --remote-name --user "${source_adminuser}:${source_password}"
+  echo "Downloading all the files that are present in Source repository and missing from the target repository to a folder '"replication_downloads"' in the current working directory"
 fi
+
+exit 0
